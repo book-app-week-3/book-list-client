d999bb6ae964f2257e1814dfea7b62c3f3454106
Catherine Looper <loopercat@gmail.com>
2017-11-07 13:52:16 -0800

    Updated README

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -1,3 +1,10 @@
 'use strict';
 
 const __API_URL__ = 'https://cl-ab-booklist.herokuapp.com';
+
+$.get('${__API_URL__}/api/v1/books')
+  .then(results => {
+    Article.loadAll(results);
+    callback();
+  })
+);
