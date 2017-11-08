d29dabd2d0762fb814676eccf5f2a814bc40b426
Catherine Looper <loopercat@gmail.com>
2017-11-07 20:40:13 -0800

    Built out index.html, book.js, and book-view.js

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -1,10 +1,32 @@
 'use strict';
 
+let app = app || {};
+
 const __API_URL__ = 'https://cl-ab-booklist.herokuapp.com';
 
-$.get('${__API_URL__}/api/v1/books')
-  .then(results => {
-    Article.loadAll(results);
-    callback();
-  })
-);
+(function(module) {
+  function errorCallback(err) {
+    console.error(err);
+    module.errorView.initErrorPage(err);
+  }
+
+  function Book (rawBookObj) {
+    Object.keys(rawBookObj).forEach(key => this[key] = rawBookObj[key]);
+  }
+
+  Book.prototype.toHtml = function() {
+    let template = Handlebars.compile($('#book-list-template').text());
+    return template(this);
+  }
+
+  Book.all = [];
+  Book.loadAll = rows => Book.all = rows.sort((a, b) => b.title - a.title).map(book => new Book(book));
+  Book.fetchAll = callback =>
+    $.get(`${__API_URL__}/api/v1/books`)
+      .then(Book.loadAll)
+      .then(callback)
+      .catch(errorCallback);
+
+  module.Book = Book;
+
+})(app)
